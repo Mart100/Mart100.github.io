d733cfda180a8e7ca890aaeab990a34487367270
midoxus <martvanenck1@gmail.com>
2018-01-14 01:45:05 +0100

    priemnumbers

--- Pages/SmallProjects/Priemnumbers/main.js
+++ Pages/SmallProjects/Priemnumbers/main.js
@@ -1,8 +1,12 @@
-for(i = 1; i < 1e99; i++) {
-  if(i % 2 == 0 || i % 3 == 0) continue
-  if(Priemgetal(i)) $('.text').append(i + '<br />')
-}
-function Priemgetal(i) {
-  for(p = 1; p < (i / 2); p += 2) if(i % p == 0 && p != 1) return false
-  return true
-}
+$(function() {
+  let i = 0
+  const calc = setInterval(function() {
+    i++
+    if(i % 2 == 0 || i % 3 == 0) i++
+    if(Priemgetal(i)) $('.text').prepend(i + '<br />')
+  }, 1)
+  function Priemgetal(i) {
+    for(p = 1; p < (i / 2); p += 2) if(i % p == 0 && p != 1) return false
+    return true
+  }
+})
