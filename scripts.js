4933e628fd0317a13de08d38898d710087ffaf82
Marto_0 <martvanenck1@gmail.com>
2018-02-10 12:23:01 +0100

    SmallProjects Update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
   $('.WhiteBOX').hover(function() {
-    $(this).css('opacity', '1')
-    $(this).css('cursor', 'pointer')
     $(this).css('width', '320')
     $(this).css('height', '370')
   }, function() {
-    $(this).css('opacity', '0.7')
     $(this).css('width', '300')
     $(this).css('height', '350')
   })
