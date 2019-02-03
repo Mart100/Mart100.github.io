a9ffdaaf0a76f20bdccd3d274ee3cbfff68a49da
Marto_0 <martvanenck1@gmail.com>
2019-02-04 00:05:08 +0100

    rainbow Background

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ $(() => {
         if(stateR > 3) stateR = 0
     }, 350)
 })
+
+// rainbow background
+$(() => {
+    let min = 10
+    let max = 50
+    let color = {r: 0, g: 0, b: 0}
+    let crrntCol = 'r'
+    setInterval(() => {
+        if(crrntCol == 'r') {
+            color.r++
+            if(color.r > max) crrntCol = 'g'
+            if(color.g > min) color.g--
+            if(color.b > min) color.b--
+        }
+        if(crrntCol == 'g') {
+            color.g++
+            if(color.g > max) crrntCol = 'b'
+            if(color.b > min) color.b--
+            if(color.r > min) color.r--
+        }
+        if(crrntCol == 'b') {
+            color.b++
+            if(color.b > max) crrntCol = 'r'
+            if(color.r > min) color.r--
+            if(color.g > min) color.g--
+        }
+        $('body').css('background-color', `rgb(${color.r}, ${color.g}, ${color.b})`)
+    }, 100)
+})
