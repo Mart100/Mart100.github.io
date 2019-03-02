ab35aa9fba91b5cc1b1f62c221c68f618f45164a
Marto_0 <martvanenck1@gmail.com>
2019-03-02 17:12:41 +0100

    njeh

--- noise/draw.js
+++
@@ -1,8 +0,0 @@
-function frame() {
-
-  // rerun frame
-  window.requestAnimationFrame(frame)
-  
-	// clear screen
-	ctx.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height)
-}
