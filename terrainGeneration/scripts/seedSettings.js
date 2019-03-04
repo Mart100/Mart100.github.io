e7aee0b31eeb9d2e6b6f0224a05f35146efb4148
Marto_0 <martvanenck1@gmail.com>
2019-03-04 14:47:21 +0100

    added grassland. And some tweaks

--- terrainGeneration/scripts/seedSettings.js
+++ terrainGeneration/scripts/seedSettings.js
@@ -3,14 +3,25 @@ let mainSeed = Math.random()
 let seeds = {
   'ocean': {
     seed: cap(mainSeed+0.0014930),
-    size: 5000,
-    plus: 2,
-    times: 150
+    size: 50000,
+    plus: 3,
+    times: 100,
+    split: {
+      at: 250,
+      plus: 500
+    }
   },
   'forest': {
     seed: cap(mainSeed+0.00112930),
     size: 500,
     plus: 2,
-    times: 100
+    times: 200
+  },
+  'grassland': {
+    seed: cap(mainSeed+0.001028930),
+    size: 500,
+    plus: 2,
+    times: 200
   }
 }
+
