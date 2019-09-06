ad5b424e3cfe73913001927e5e412f07a73cb93d
Marto_0 <martvanenck1@gmail.com>
2019-09-07 01:36:29 +0200

    Moosic Site

---
+++ Moosic/script.js
@@ -0,0 +1,9 @@
+$(() => {
+  var $root = $('html, body');
+
+  $('a[href^="#"]').click((event) => {
+    let href = $($(event.target).attr('href'))
+    $('body').animate({scrollTop: $(href).offset().top}, 500, () => window.location.hash = href)
+    return false
+  })
+})
