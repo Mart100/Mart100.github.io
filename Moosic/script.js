28cd872cae24c1c669ebed8b3c9f0b42202c8333
Marto_0 <martvanenck1@gmail.com>
2019-09-07 13:48:40 +0200

    things

--- Moosic/script.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-  var $root = $('html, body')
-
-  $('a[href^="#"]').click((event) => {
-    let href = $($(event.target).attr('href'))
-    $('body').animate({scrollTop: $(href).offset().top}, 500, () => window.location.hash = $(event.target).attr('href'))
-    return false
-  })
-})
